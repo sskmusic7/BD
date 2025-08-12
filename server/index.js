44fe8719cfd8bdea424d58c8a524d00565540d9d
sskmusic7 <sskmusic7@gmail.com>
2025-08-13 00:25:22 +0100

    Add global CORS middleware before all routes - v2.4

--- server/index.js
+++ server/index.js
@@ -7,6 +7,15 @@ require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
+
+// Force CORS headers on ALL responses
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const io = socketIo(server, {
   cors: {
     origin: "*",
